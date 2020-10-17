c65665ba6cac1d41ef020bd50c17e6ab1697bbf4
rohansharma06 <rohansharma251998@gmail.com>
2020-10-17 11:42:27 +0530

    create itemdisplay componwnt

---
+++ src/component/ListItemDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListItemDisplay extends Component {
+    render() {
+        return (
+            <div className="display-container">
+                <h1>Display</h1>
+            </div>
+        );
+    }
+}
+
+export default ListItemDisplay;
